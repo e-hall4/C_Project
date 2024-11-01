a73e25bef0c220fb7723ece09cd93109edcf8944
warock7 <wr00009@mix.wvu.edu>
2024-11-01 10:46:13 -0400

    Fixed LUI to check corrct bits. Should now word as intended.

--- LUI.c
+++ LUI.c
@@ -26,13 +26,13 @@ void lui_immd_assm(void) {
 
     // Putting the binary together
     // Set the opcode for LUI (001111)
-    setBits_str(26, "001111");
+    setBits_str(31, "001111");
 
     // Set Rt (destination register)
-    setBits_num(21, PARAM1.value, 5);
+    setBits_num(20, PARAM1.value, 5);
 
     // Set the immediate value (16 bits)
-    setBits_num(0, PARAM2.value, 16); // Assuming PARAM2.value is a signed 16-bit integer
+    setBits_num(15, PARAM2.value, 16); // Assuming PARAM2.value is a signed 16-bit integer
 
     // Tell the system the encoding is done
     state = COMPLETE_ENCODE;
@@ -40,7 +40,7 @@ void lui_immd_assm(void) {
 
 void lui_immd_bin(void) {
     // Check if the op code bits match
-    if (checkBits(26, "001111") != 0) {
+    if (checkBits(31, "001111") != 0) {
         state = WRONG_COMMAND;
         return;
     }
@@ -48,8 +48,8 @@ void lui_immd_bin(void) {
     // If the op code bits match, then the rest can be read as correctly
 
     // Finding values in the binary
-    uint32_t Rt = getBits(21, 5); // destination register
-    uint32_t immediate = getBits(0, 16); // immediate value
+    uint32_t Rt = getBits(20, 5); // destination register
+    uint32_t immediate = getBits(15, 16); // immediate value
 
     // Setting Instruction values
     setOp("LUI");
